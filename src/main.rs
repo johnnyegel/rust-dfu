943a0bb166ada50edf41146908febebe3e3b2cb4
Johnny Egeland <johnnyege@gmail.com>
2021-04-08 21:33:27 +0200

    Committed last changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod util;
 
-use std::path::Path;
+use std::{any::Any, path::Path};
 use std::ffi::OsStr;
 use clap::{Arg, App};
 
@@ -26,7 +26,7 @@ fn main() {
     let appdef = 
         App::new(APP_NAME)
             .version(VERSION)
-            .author("Johnny Egeland")
+            .author("Created by: Johnny Egeland (c) 2021")
             .about("Utility to upload Firmware images to DFU capable hardware. Supports a number of formats: iHEX, ELF, DFU and BIN")
             .arg(Arg::with_name("format")
                 .short("f")
@@ -67,10 +67,74 @@ fn main() {
         None => parse_image_type_from_extension(&get_file_extension(&fw_image_file, "elf"), fw_offset)
     };
 
-
     println!("Using image file: {}", fw_image_file);
     println!("Using format: {:?} @ 0x{:08X}", fw_image_type, fw_offset.unwrap_or(0));
-    
+
+    // Some simple USB enumeration here.
+    for device in rusb::devices().unwrap().iter() {
+
+        // Get the device descriptor
+        let device_desc_result = device.device_descriptor();
+        if let Err(e) = device_desc_result {
+            println!("Unable to get device descriptor: {}", e);
+            continue;
+        }
+
+        // Make sure there is a active configuration
+        let active_config_result = device.active_config_descriptor();
+        if let Err(e) = active_config_result {
+            println!("Unable to get active config: {}", e);
+            continue;
+        }
+
+        // Now unwrap should work
+        let device_desc = device_desc_result.unwrap();
+        let active_config = active_config_result.unwrap();
+
+        // Grab the interfaces
+        let interfaces = active_config.interfaces();
+        let mut is_device_listed = false;
+
+        for interface in interfaces.into_iter() {
+            for if_desc in interface.descriptors().into_iter() {
+                // Skip interfaces which are not DFU
+                if if_desc.class_code() != 0xFE || if_desc.sub_class_code() != 0x01 { 
+                    continue; 
+                }
+
+                // Display the device if not already done
+                if !is_device_listed {
+
+                    println!("Bus {:03} Device {:03} ID {:04x}:{:04x}, Class {:02X}:{:02X}",
+                        device.bus_number(),
+                        device.address(),
+                        device_desc.vendor_id(),
+                        device_desc.product_id(),
+                        device_desc.class_code(),
+                        device_desc.sub_class_code()
+                    );
+
+                    is_device_listed = true;
+                }
+
+                let string_index = if_desc.description_string_index().unwrap_or(0xFF);
+
+                // Display interface info
+                println!(" - Interface [{}]: Class: {:02X}:{:02X}, Protocol: {:02X}, String: {:02X}",
+                    interface.number(),
+                    if_desc.class_code(),
+                    if_desc.sub_class_code(),
+                    if_desc.protocol_code(),
+                    string_index
+                );
+
+
+            }
+        }
+
+    }
+
+
 }
 
 /// Returns the file extension in lower case, or the default value as a string
